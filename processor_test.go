qluvio/asynq

commit 7b11cbb588c8c4f52f4eb394e3cfdc892f757166

add 'after' hook to execute after task state change (#20)
* add 'after' hook to execute after task execution
* error & delay handling to use same logic as task handling based on task type

server.go
@@ -66,7 +66,7 @@ type Config struct {
 	// Function to calculate retry delay for a failed task.
 	//
 	// By default, it uses exponential backoff algorithm to calculate the delay.
-	RetryDelayFunc RetryDelayFunc
+	RetryDelayFunc RetryDelayHandler
 
 	// Predicate function to determine whether the error returned from Handler is a failure.
 	// If the function returns false, Server will not increment the retried counter for the task,
@@ -158,7 +158,7 @@ type Config struct {
 	JanitorInterval time.Duration
 }
 
-// An ErrorHandler handles an error occured during task processing.
+// An ErrorHandler handles an error that occurred during task processing.
 type ErrorHandler interface {
 	HandleError(ctx context.Context, task *Task, err error)
 }
@@ -172,6 +172,10 @@ func (fn ErrorHandlerFunc) HandleError(ctx context.Context, task *Task, err erro
 	fn(ctx, task, err)
 }
 
+type RetryDelayHandler interface {
+	RetryDelay(n int, e error, t *Task) time.Duration
+}
+
 // RetryDelayFunc calculates the retry delay duration for a failed task given
 // the retry count, error, and the task.
 //
@@ -179,6 +183,10 @@ func (fn ErrorHandlerFunc) HandleError(ctx context.Context, task *Task, err erro
 // e is the error returned by the task handler.
 // t is the task in question.
 type RetryDelayFunc func(n int, e error, t *Task) time.Duration
+
+func (fn RetryDelayFunc) RetryDelay(n int, e error, t *Task) time.Duration {
+	return fn(n, e, t)
+}
 
 // Logger supports logging at various log levels.
 type Logger interface {
@@ -291,6 +299,8 @@ func DefaultRetryDelayFunc(n int, e error, t *Task) time.Duration {
 	return time.Duration(s) * time.Second
 }
 
+var DefaultRetryDelay = RetryDelayFunc(DefaultRetryDelayFunc)
+
 func defaultIsFailureFunc(err error) bool { return err != nil }
 
 const (
@@ -320,7 +330,7 @@ func newServer(broker base.Broker, cfg Config) *Server {
 	}
 	delayFunc := cfg.RetryDelayFunc
 	if delayFunc == nil {
-		delayFunc = DefaultRetryDelayFunc
+		delayFunc = DefaultRetryDelay
 	}
 	isFailureFunc := cfg.IsFailure
 	if isFailureFunc == nil {
@@ -380,6 +390,7 @@ func newServer(broker base.Broker, cfg Config) *Server {
 	syncCh := make(chan *syncRequest)
 	state := base.NewServerState()
 	cancels := base.NewCancelations()
+	afterTasks := base.NewAfterTasks()
 
 	syncer := newSyncer(syncerParams{
 		logger:     logger,
@@ -411,6 +422,7 @@ func newServer(broker base.Broker, cfg Config) *Server {
 		isFailureFunc:   isFailureFunc,
 		syncCh:          syncCh,
 		cancelations:    cancels,
+		afterTasks:      afterTasks,
 		concurrency:     n,
 		queues:          cfg.Queues,
 		errHandler:      cfg.ErrorHandler,

processor_test.go
@@ -25,7 +25,7 @@ import (
 var taskCmpOpts = []cmp.Option{
 	sortTaskOpt,                 // sort the tasks
 	cmp.AllowUnexported(Task{}), // allow typename, payload fields to be compared
-	cmpopts.IgnoreFields(Task{}, "opts", "w", "p"), // ignore opts, w, p fields
+	cmpopts.IgnoreFields(Task{}, "opts", "w", "p", "callAfter"), // ignore opts, w, p fields
 }
 
 // fakeHeartbeater receives from starting and finished channels and do nothing.
@@ -63,7 +63,7 @@ func newProcessorForTest(t *testing.T, r base.Broker, h Handler) *processor {
 	p := newProcessor(processorParams{
 		logger:          testLogger,
 		broker:          r,
-		retryDelayFunc:  DefaultRetryDelayFunc,
+		retryDelayFunc:  DefaultRetryDelay,
 		isFailureFunc:   defaultIsFailureFunc,
 		syncCh:          syncCh,
 		cancelations:    base.NewCancelations(),
@@ -593,9 +593,9 @@ func TestProcessorRetry(t *testing.T) {
 			ctx.SeedPendingQueue(tc.pending, base.DefaultQueueName) // initialize default queue.
 
 			// instantiate a new processor
-			delayFunc := func(n int, e error, t *Task) time.Duration {
+			delayFunc := RetryDelayFunc(func(n int, e error, t *Task) time.Duration {
 				return tc.delay
-			}
+			})
 			var (
 				mu sync.Mutex // guards n
 				n  int        // number of times error handler is called
@@ -607,7 +607,7 @@ func TestProcessorRetry(t *testing.T) {
 			}
 			p := newProcessorForTest(t, client.rdb, tc.handler)
 			p.errHandler = ErrorHandlerFunc(errHandler)
-			p.retryDelayFunc = delayFunc
+			p.retryDelay = delayFunc
 
 			p.start(&sync.WaitGroup{})
 			runTime := time.Now() // time when processor is running
@@ -855,7 +855,7 @@ func TestProcessorWithStrictPriority(t *testing.T) {
 			p := newProcessor(processorParams{
 				logger:          testLogger,
 				broker:          client.rdb,
-				retryDelayFunc:  DefaultRetryDelayFunc,
+				retryDelayFunc:  DefaultRetryDelay,
 				isFailureFunc:   defaultIsFailureFunc,
 				syncCh:          syncCh,
 				cancelations:    base.NewCancelations(),

asynq.go
@@ -35,6 +35,9 @@ type Task struct {
 
 	// p is the AsyncProcessor for the task.
 	p *AsyncProcessor
+
+	// callAfter lets task processing schedule a function call after task execution
+	callAfter func(fn func(string, error, bool))
 }
 
 func (t *Task) Type() string    { return t.typename }
@@ -52,6 +55,19 @@ func (t *Task) ResultWriter() *ResultWriter { return t.w }
 // Only the tasks passed to Handler.ProcessTask have a valid AsyncProcessor pointer.
 func (t *Task) AsyncProcessor() *AsyncProcessor { return t.p }
 
+// CallAfter enables task processing to schedule execution of a function after the
+// task execution. The function is guaranteed to be called once the task state has
+// changed in the broker database.
+// Function parameter:
+// string: task id
+// error: is the error returned by task execution.
+// bool: indicates if the error - if not nil - was considered as an actual error
+func (t *Task) CallAfter(fn func(string, error, bool)) {
+	if t.callAfter != nil {
+		t.callAfter(fn)
+	}
+}
+
 // NewTask returns a new Task given a type name and payload data.
 // Options can be passed to configure task processing behavior.
 func NewTask(typename string, payload []byte, opts ...Option) *Task {
@@ -63,12 +79,13 @@ func NewTask(typename string, payload []byte, opts ...Option) *Task {
 }
 
 // newTask creates a task with the given typename, payload, ResultWriter, and AsyncProcessor.
-func newTask(typename string, payload []byte, w *ResultWriter, p *AsyncProcessor) *Task {
+func newTask(typename string, payload []byte, w *ResultWriter, p *AsyncProcessor, ca func(fn func(string, error, bool))) *Task {
 	return &Task{
-		typename: typename,
-		payload:  payload,
-		w:        w,
-		p:        p,
+		typename:  typename,
+		payload:   payload,
+		w:         w,
+		p:         p,
+		callAfter: ca,
 	}
 }
 

asynq_end2end_sqlite_test.go
@@ -42,9 +42,9 @@ func TestLocalLongRun(t *testing.T) {
 	srv := newServer(client.rdb, Config{
 		ServerID:    "inod1111",
 		Concurrency: 10,
-		RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+		RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 			return time.Millisecond * 200
-		},
+		}),
 		LogLevel:             InfoLevel,
 		ProcessorEmptyQSleep: time.Millisecond * 200,
 		ShutdownTimeout:      time.Millisecond * 10,

asynq_end2end_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"sort"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -25,9 +26,9 @@ func TestEndToEnd(t *testing.T) {
 	client := NewClient(getClientConnOpt(t))
 	srv := newServer(client.rdb, Config{
 		Concurrency: 10,
-		RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+		RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 			return time.Second
-		},
+		}),
 		LogLevel: testLogLevel,
 	})
 	defer srv.Shutdown()
@@ -89,9 +90,9 @@ func TestEndToEndAsync(t *testing.T) {
 	client := NewClient(getClientConnOpt(t))
 	srv := newServer(client.rdb, Config{
 		Concurrency: 10,
-		RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+		RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 			return time.Second
-		},
+		}),
 		LogLevel: testLogLevel,
 	})
 	defer srv.Shutdown()
@@ -177,9 +178,9 @@ func doTestEndToEndRestartWithActiveTasks(t *testing.T, regularShutdown bool) {
 		client := NewClient(getClientConnOpt(t))
 		srv := newServer(client.rdb, Config{
 			Concurrency: 10,
-			RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+			RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 				return time.Second * 10
-			},
+			}),
 			LogLevel:             DebugLevel,
 			ProcessorEmptyQSleep: time.Millisecond * 200,
 			ShutdownTimeout:      time.Millisecond * 10,
@@ -239,9 +240,9 @@ func doTestEndToEndRestartWithActiveTasks(t *testing.T, regularShutdown bool) {
 		client := NewClient(getClientConnOpt(t))
 		srv := newServer(client.rdb, Config{
 			Concurrency: 10,
-			RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+			RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 				return time.Second
-			},
+			}),
 			LogLevel:             DebugLevel,
 			ProcessorEmptyQSleep: time.Millisecond * 200,
 			ShutdownTimeout:      time.Millisecond * 10,
@@ -292,9 +293,9 @@ func TestCancelProcessing(t *testing.T) {
 	client := NewClient(getClientConnOpt(t))
 	srv := newServer(client.rdb, Config{
 		Concurrency: 10,
-		RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+		RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 			return time.Second
-		},
+		}),
 		HeartBeaterInterval: time.Millisecond * 10,
 		LogLevel:            testLogLevel,
 	})
@@ -363,4 +364,69 @@ func TestCancelProcessing(t *testing.T) {
 
 	srv.Stop()
 	_ = client.Close()
+}
+
+// TestAfterTask
+func TestAfterTask(t *testing.T) {
+	ctx := setupTestContext(t)
+	defer func() { _ = ctx.Close() }()
+
+	ctx.FlushDB()
+
+	client := NewClient(getClientConnOpt(t))
+	srv := newServer(client.rdb, Config{
+		Concurrency: 10,
+		LogLevel:    testLogLevel,
+	})
+	defer srv.Shutdown()
+
+	// Create a bunch of tasks
+	for i := 0; i < 10; i++ {
+		if _, err := client.Enqueue(makeTask(i)); err != nil {
+			t.Fatalf("could not enqueue a task: %v", err)
+		}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	after := []int(nil)
+	mu := sync.Mutex{}
+
+	received := func(i int) {
+		mu.Lock()
+		defer mu.Unlock()
+		after = append(after, i)
+	}
+
+	handler := func(ctx context.Context, task *Task) error {
+		defer wg.Done()
+		m := map[string]int{}
+		err := json.Unmarshal(task.payload, &m)
+		if err != nil {
+			t.Fatalf("unmarshal task: %v", err)
+		}
+		indx, ok := m["data"]
+		if !ok {
+			t.Fatalf("invalid payload: %v", string(task.payload))
+		}
+
+		if indx%2 == 0 {
+			task.CallAfter(func(string, error, bool) {
+				received(indx)
+			})
+		}
+		return nil
+	}
+
+	_ = srv.Start(HandlerFunc(handler))
+	wg.Wait()
+	srv.Stop()
+
+	// do the comparison after stopping the server to finish active tasks
+	sort.Ints(after)
+	require.Equal(t, []int{0, 2, 4, 6, 8}, after)
+
+	_ = client.Close()
+
 }

benchmark_test.go
@@ -38,9 +38,9 @@ func BenchmarkEndToEndSimple(b *testing.B) {
 
 		srv := newServer(client.rdb, Config{
 			Concurrency: 10,
-			RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+			RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 				return time.Second
-			},
+			}),
 			LogLevel: testLogLevel,
 		})
 		// Create a bunch of tasks
@@ -80,9 +80,9 @@ func BenchmarkEndToEnd(b *testing.B) {
 		client := NewClient(getClientConnOpt(b))
 		srv := newServer(client.rdb, Config{
 			Concurrency: 10,
-			RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+			RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 				return time.Second
-			},
+			}),
 			LogLevel: testLogLevel,
 		})
 		// Create a bunch of tasks
@@ -210,9 +210,9 @@ func BenchmarkClientWhileServerRunning(b *testing.B) {
 		client := NewClient(getClientConnOpt(b))
 		srv := newServer(client.rdb, Config{
 			Concurrency: 10,
-			RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+			RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 				return time.Second
-			},
+			}),
 			LogLevel: testLogLevel,
 		})
 		// Enqueue 10,000 tasks.

errmux.go
@@ -1,0 +1,44 @@
+package asynq
+
+import "context"
+
+type ErrorMux struct {
+	taskMatcher *TaskMatcher[ErrorHandler]
+	defHandler  func() ErrorHandler
+}
+
+func NewErrorMux(defaultHandler ErrorHandler) *ErrorMux {
+	def := noErrorHandler
+	if defaultHandler != nil {
+		def = func() ErrorHandler { return defaultHandler }
+	}
+	return &ErrorMux{
+		taskMatcher: NewTaskMatcher[ErrorHandler](),
+		defHandler:  def,
+	}
+}
+
+func (mux *ErrorMux) HandleError(ctx context.Context, task *Task, err error) {
+	h, _ := mux.Handler(task)
+	h.HandleError(ctx, task, err)
+}
+
+func (mux *ErrorMux) Handler(t *Task) (h ErrorHandler, pattern string) {
+	return mux.taskMatcher.Handler(t, mux.defHandler)
+}
+
+func (mux *ErrorMux) Handle(pattern string, handler ErrorHandler) {
+	mux.taskMatcher.Handle(pattern, handler)
+}
+
+func (mux *ErrorMux) HandleFunc(pattern string, handler func(context.Context, *Task, error)) {
+	if handler == nil {
+		panic("asynq: nil error handler")
+	}
+	mux.Handle(pattern, ErrorHandlerFunc(handler))
+}
+
+// noErrorHandler returns a noop error handler.
+func noErrorHandler() ErrorHandler {
+	return ErrorHandlerFunc(func(ctx context.Context, task *Task, err error) {})
+}

inspector_test.go
@@ -3365,9 +3365,9 @@ func TestInspectorCancelProcessing(t *testing.T) {
 
 			server := newServer(client.rdb, Config{
 				Concurrency: 10,
-				RetryDelayFunc: func(n int, err error, t *Task) time.Duration {
+				RetryDelayFunc: RetryDelayFunc(func(n int, err error, t *Task) time.Duration {
 					return time.Second
-				},
+				}),
 				LogLevel:            testLogLevel,
 				HeartBeaterInterval: time.Millisecond * 5,
 			})

commit 5d667457e055b301e877655f0c1ce924ac7ca960

fix nil check of RetryDelayFunc (#21)
* issue introduced in #20

server.go
@@ -329,7 +329,7 @@ func newServer(broker base.Broker, cfg Config) *Server {
 		cfg.ServerID = uuid.New().String()
 	}
 	delayFunc := cfg.RetryDelayFunc
-	if delayFunc == nil {
+	if IsNil(delayFunc) {
 		delayFunc = DefaultRetryDelay
 	}
 	isFailureFunc := cfg.IsFailure

processor_test.go
@@ -20,6 +20,7 @@ import (
 	h "github.com/hibiken/asynq/internal/asynqtest"
 	"github.com/hibiken/asynq/internal/base"
 	"github.com/hibiken/asynq/internal/timeutil"
+	"github.com/stretchr/testify/require"
 )
 
 var taskCmpOpts = []cmp.Option{
@@ -651,6 +652,24 @@ func TestProcessorRetry(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestProcessorNilRetry(t *testing.T) {
+	ctx := setupTestContext(t)
+	defer func() { _ = ctx.Close() }()
+
+	client := NewClient(getClientConnOpt(t))
+	defer func() { _ = client.Close() }()
+
+	task := NewTask("", nil)
+
+	var retryDelayFunc func(n int, e error, t *Task) time.Duration
+	s := newServer(client.rdb, Config{
+		RetryDelayFunc: RetryDelayFunc(retryDelayFunc),
+	})
+	d := s.processor.retryDelay.RetryDelay(1, nil, task)
+
+	require.True(t, d > 0)
 }
 
 func TestProcessorMarkAsComplete(t *testing.T) {